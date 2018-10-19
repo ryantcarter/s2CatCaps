df6181dca7148d2e9a03fdc1daabe161936f446f
Charles Harries <charlesgharries@gmail.com>
2018-10-19 11:04:13 +0100

    Update to WordPress Coding Standards

--- s2member-auto-ccaps.php
+++ s2member-auto-ccaps.php
@@ -17,7 +17,7 @@ class C_s2member_Auto_Custom_Capabilities
     static $_instance = NULL;
     static function get_instance()
     {
-        if (!isset(self::$_instance)) {
+        if ( ! isset( self::$_instance ) ) {
             $klass = get_class();
             self::$_instance = new $klass;
         }
@@ -29,38 +29,38 @@ class C_s2member_Auto_Custom_Capabilities
      */
     function __construct()
     {
-        add_action('save_post', array(&$this, 'associate_custom_capabilities'), PHP_INT_MAX-2);
+        add_action( 'save_post', array(&$this, 'associate_custom_capabilities'), PHP_INT_MAX );
     }
 
     /**
      * After a post has been saved, automatically associate the custom capabilities for the post, as indicated
      * by the categories assigned
      */
-    function associate_custom_capabilities($post_id)
+    function associate_custom_capabilities( $post_id )
     {
         $ccap_field = 's2member_ccaps_req';
-        $categories = wp_get_post_categories($post_id);
-        $ccaps = get_post_meta($post_id, $ccap_field, TRUE);
+        $categories = wp_get_post_categories( $post_id );
+        $ccaps = get_post_meta( $post_id, $ccap_field, TRUE );
 
         // Iterate through each category_id, get the slug of the category,
         // and then add that slug as a custom capability for the post
-        foreach ($categories as $category_id) {
-            $category = get_category($category_id);
-            if (!in_array($category->slug, $ccaps)) {
+        foreach ( $categories as $category_id ) {
+            $category = get_category( $category_id );
+            if ( ! in_array( $category->slug, $ccaps ) ) {
                 $ccaps[] = $category->slug;
             }
         }
 
         // Update the the custom capabilities field for the post
         global $wpdb;
-        $wpdb->delete($wpdb->postmeta, array(
+        $wpdb->delete( $wpdb->postmeta, array(
             'post_id'   =>  $post_id,
             'meta_key'  =>  $ccap_field
         ));
-        $wpdb->insert($wpdb->postmeta, array(
+        $wpdb->insert( $wpdb->postmeta, array(
            'post_id'    =>  $post_id,
            'meta_key'   =>  $ccap_field,
-           'meta_value' =>  serialize($ccaps)
+           'meta_value' =>  serialize( $ccaps )
         ));
     }
 }
